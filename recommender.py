e3c2bac10be184acc26e72bc06218540791a8d5b
RiccardoAncarani <riccardoancarani94@gmail.com>
2017-03-15 20:52:49 +0100

    implemented knn unsupervised learning

--- recommender.py
+++ recommender.py
@@ -1,5 +1,6 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import random
 
 class VectorDimensionError(Exception):
     def __init__(self, message, errors):
@@ -12,9 +13,21 @@ def calculateSimilarity(x, y):
 		raise VectorDimensionError("Vector must be the same size",None)
 	return cosine_similarity(np.array(x).reshape(1,len(x)),np.array(y).reshape(1,len(y)))
 
-def main():
-	print calculateSimilarity([0,1,0,1,0],[1,2,5,4])
+def createJunkData():
+	return np.array([generateJunkVector(5) for i in range(5)]).T
 
+def tryKNN():
+	from sklearn.neighbors import NearestNeighbors
+	import numpy as np
+	X = createJunkData()
+	nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
+	distances, indices = nbrs.kneighbors([0,1,2,1,5], n_neighbors=1)
+	print distances
+	print X[indices]
+
+
+def generateJunkVector(n):
+	return [random.random() for i in range(n)]
 
 if __name__ == '__main__':
-	main()
+	tryKNN()
